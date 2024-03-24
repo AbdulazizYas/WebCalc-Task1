91d4cbfddeb0955920e76deb9aac6d92dd2a1698
AbdulazizYas <abdulazizyass@gmail.com>
2024-03-24 23:36:05 +0300

    init the files

---
+++ div.js
@@ -0,0 +1,11 @@
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('div', (req, res) => {
+  const {X, Y} = req.body
+
+  res.json({
+    X,
+    Y,
+    Result: X / Y
+  })
+});
